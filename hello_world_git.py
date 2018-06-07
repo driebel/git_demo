774dd6350ecb74bbd70359defa26d7227f0980a3
David Riebel <riebel.d@gmail.com>
2018-06-06 18:25:47 -0700

    Initial commit.

---
+++ hello_world_git.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  6 18:19:46 2018
+
+@author: driebel
+"""
+
+def hello_world_git():
+    print("I'm on GIT!")
+    
+if __name___ == "__main__":
+    print(hello_world_git())
